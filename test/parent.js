d51f3a68cc5168405d54a8a9210fea70b0849a45
mponizil <misha.ponizil@gmail.com>
2014-05-08 18:44:24 -0400

    Declare vars at the top

--- test/parent.js
+++ test/parent.js
@@ -27,11 +27,13 @@ module.exports = function() {
   test('Transport correctly defines level.', function(t) {
     t.plan(2);
 
-    var parent = new IFT.Parent(config.IFT_HOST, config.IFT_PATH);
+    var parent, child;
+
+    parent = new IFT.Parent(config.IFT_HOST, config.IFT_PATH);
     t.equal(parent.level, 'parent');
     parent.destroy();
 
-    var child = new IFT.Child;
+    child = new IFT.Child;
     t.equal(child.level, 'child');
     child.destroy();
 
@@ -41,8 +43,10 @@ module.exports = function() {
   test('Invoke and callback.', function(t) {
     t.plan(1);
 
-    var ift = new IFT.Parent(config.IFT_HOST, config.IFT_PATH, 'test', function() {
-      var client = new IFT.Client(ift);
+    var ift, client;
+
+    ift = new IFT.Parent(config.IFT_HOST, config.IFT_PATH, 'test', function() {
+      client = new IFT.Client(ift);
       client.send('invoke', 'test', [], function() {
         t.pass('Acknowledged.');
         ift.destroy();
@@ -54,15 +58,17 @@ module.exports = function() {
   test('Trigger.', function(t) {
     t.plan(1);
 
-    var ift = new IFT.Parent(config.IFT_HOST, config.IFT_PATH, 'test', function() {
-      var TestClient = IFT.Client.extend({
-        ack: function() {
-          t.pass('Acknowledged.');
-          ift.destroy();
-          t.end();
-        }
-      });
-      var client = new TestClient(ift);
+    var ift, client;
+    var TestClient = IFT.Client.extend({
+      ack: function() {
+        t.pass('Acknowledged.');
+        ift.destroy();
+        t.end();
+      }
+    });
+
+    ift = new IFT.Parent(config.IFT_HOST, config.IFT_PATH, 'test', function() {
+      client = new TestClient(ift);
       client.send('trigger', 'test', []);
     });
   });

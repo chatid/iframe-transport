9544ca7327479b820f699208b6178b1efb61a65a
Misha Ponizil <misha.ponizil@gmail.com>
2015-05-12 23:56:58 -0400

    Fix examples

--- example/child.js
+++
@@ -1,7 +0,0 @@
-var ift = require('../library/services/storage');
-
-ift.child({
-  trustedOrigins: ['http://127.0.0.1:8000']
-}).wiretap(function(direction, message) {
-  ift.util.debug(direction, message);
-}).service('example', IFTStorageService.Provider);
--- webpack.config.js
+++ webpack.config.js
@@ -4,22 +4,21 @@ var webpack = require('webpack'),
 module.exports = {
   devtool: 'source-map',
   entry: {
-    'ift': ['./library/ift.js'],
-    'storage': ['./library/services/storage.js'],
-    'storage-compat': ['./library/services/storage-compat.js'],
-    'example-parent': './example/parent.js',
-    'example-child': './example/child.js'
+    'ift': './library/ift',
+    'IFTStorageService': './library/services/storage'
   },
   output: {
-    path: __dirname +'/build',
-    filename: '[name].js'
+    path: __dirname + '/build',
+    filename: '[name].js',
+    library: '[name]',
+    libraryTarget: 'umd'
   },
   resolve: {
-    root: [path.join(__dirname, "bower_components")]
+    root: [path.join(__dirname, 'bower_components')]
   },
   plugins: [
     new webpack.ResolverPlugin(
-      new webpack.ResolverPlugin.DirectoryDescriptionFilePlugin("bower.json", ["main"])
+      new webpack.ResolverPlugin.DirectoryDescriptionFilePlugin('bower.json', ['main'])
     )
   ]
 };

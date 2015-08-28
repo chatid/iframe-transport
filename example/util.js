c158c5d960316668288394d6c934e68a0b3cc4c2
phillyqueso <phillip@chatid.com>
2015-08-28 14:43:26 -0500

    adding paths helper

--- example/util.js
+++ example/util.js
@@ -1,3 +1,11 @@
 var getChildOptions = function() {
   if (location.origin.match(/github/)) { return "chatid.github.io" } else { return "127.0.0.1:8000" }
 }
+
+var getUrlPath = function(path) {
+  var firstLocationPath = location.pathname.split("/")[0];
+  var pathArr = path.split("/");
+  if (pathArr[0] !== firstLocationPath)
+    return "/" + firstLocationPath + path;
+  return path;
+}

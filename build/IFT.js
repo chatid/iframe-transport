3494fb939302ab404fa8977d2686a764ee0c22db
phillyqueso <phillip@chatid.com>
2016-03-25 13:14:51 -0500

    module.exports instead of export default

--- build/IFT.js
+++ build/IFT.js
@@ -46,10 +46,6 @@
 
 	'use strict';
 
-	Object.defineProperty(exports, "__esModule", {
-	  value: true
-	});
-
 	function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
 	// [I][F]rame [T]ranspot
@@ -134,7 +130,7 @@
 	;
 
 	var iftInstance = new IFT();
-	exports.default = iftInstance;
+	module.exports = iftInstance;
 
 /***/ }
 /******/ ]);
